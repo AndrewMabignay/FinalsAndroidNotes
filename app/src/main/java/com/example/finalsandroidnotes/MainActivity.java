ea0bbfc0ba9f971c4879a4197e4e356d5bd795cb
Andrew <andrewmabignay49@gmail.com>
2025-04-24 07:15:18 +0800

    First Commit Firebase Database

---
+++ app/src/main/java/com/example/finalsandroidnotes/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.finalsandroidnotes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Sample Hello World Trinx");
+    }
+}
